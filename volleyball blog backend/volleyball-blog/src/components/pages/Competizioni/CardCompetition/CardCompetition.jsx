6075f6c1e8d83acc6dbf2f19f0f1efdfe60b8a8c
peterlyon02 <pietro.filosa2002@gmail.com>
2024-07-13 02:58:30 +0200

    upload

--- volleyball blog backend/volleyball-blog/src/components/pages/Competizioni/CardCompetition/CardCompetition.jsx
+++ volleyball blog backend/volleyball-blog/src/components/pages/Competizioni/CardCompetition/CardCompetition.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom"
 import './CardCompetition.css'
 function CardCompetition(prop){
     return (<Link to={`/competizioni/${prop.idLeague}`} className="text-decoration-none">
-        <div className='card '>
+        <div className=' card-competition card'>
           <div className="container-title">
             <h1 className='text-center'>{prop.strLeague}</h1>
           </div>
-          <img src={prop.strBadge} className='img-fluid '></img>
+          <img src={prop.strBadge} className=' img-competition '></img>
           <p className="description">{prop.strDescriptionEN}</p>
         </div>
         </Link>)
--- volleyball blog backend/volleyball-blog/src/components/pages/Competizioni/Competizioni.jsx
+++ volleyball blog backend/volleyball-blog/src/components/pages/Competizioni/Competizioni.jsx
@@ -3,6 +3,7 @@ import React  from 'react'
 import { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import CardCompetition from './CardCompetition/CardCompetition'
+import './Competizioni.css'
 
 function Competizioni(){
     const [leagues, setLeagues]=useState([])
@@ -20,9 +21,12 @@ function Competizioni(){
       }, [])
 
     return(
-        <div className='row'>
-            {leagues.map((league) => (
-                <div className='col-3 p-2'>
+        <div className='main-container'>
+          <div className='container pt-3 '>
+            <h5 className='text-light'>Division 1</h5>
+            <div className='row'>
+            {leagues.slice(0,7).map((league) => (
+                <div className='col-sm-6 col-lg-3 col-md-4 p-2' >
                    <CardCompetition 
                     idLeague={league.idLeague}
                     strLeague={league.strLeague}
@@ -31,8 +35,25 @@ function Competizioni(){
                    /> 
                 </div>
             ))};
+            </div>
+            <h5 className='text-light pt-4'>Cup Competition </h5>
+            <div className='row'>
+            {leagues.slice(7).map((league) => (
+                <div className='col-sm-6 col-lg-3 col-md-4 p-2' >
+                   <CardCompetition 
+                    idLeague={league.idLeague}
+                    strLeague={league.strLeague}
+                    strBadge={league.strBadge}
+                    strDescriptionEN={league.strDescriptionEN}
+                   /> 
+                </div>
+            ))};
+            </div>
+          </div>
         </div>
     )
 }
 
 export default Competizioni
+
+
