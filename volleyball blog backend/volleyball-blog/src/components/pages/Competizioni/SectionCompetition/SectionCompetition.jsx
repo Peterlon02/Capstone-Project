4c286e3f4bb992afc737aecd60cda10658a11436
peterlyon02 <pietro.filosa2002@gmail.com>
2024-07-13 14:55:48 +0200

    upload

--- volleyball blog backend/volleyball-blog/src/components/pages/Competizioni/SectionCompetition/SectionCompetition.jsx
+++ volleyball blog backend/volleyball-blog/src/components/pages/Competizioni/SectionCompetition/SectionCompetition.jsx
@@ -4,12 +4,10 @@ import axios from "axios";
 import allEvents from './dictEvents/dictEvents'
 
 function SectionCompetition(){
-    const {competitionId}=useParams()
-    const [teams, setTeams]=useState([])
-    const [choise, setChoise]=useState('')
-    const [seasons, setSeasons]=useState([])
-    const [choiseSeason, setChoiseSeason] = useState('');
-    const [eventsByRound, setEventsByRound] = useState({});
+    const {competitionId}=useParams()/* Id della competizione */
+    const [teams, setTeams]=useState([])/* teams di quella competizione */
+    const [seasons, setSeasons]=useState([])/*lista delle seasons di  quella competizione */
+    const [eventsByRound, setEventsByRound] = useState({});/*giornate della season(DA RIVEDERE MEGLIO PERCHE' SONO SBALLATE) */
 
   useEffect(() => {
     const getEvents = async () => {
@@ -44,68 +42,8 @@ function SectionCompetition(){
         fetchSeasons()
       }, [])
 
-    const handleTeamsClick=()=>{
-      setChoise('teams')
-    }
-
-    const handleEventClick=()=>{
-      setChoise('event')
-    }
-
-    const handleSeasonClick=(season)=>{
-      setChoiseSeason(season)
-    }
-
     return (
-        <div>
-          <div className="row ">
-            <div className="col-6">
-            <button onClick={handleTeamsClick} className="w-100 ">Teams</button>
-            </div>
-            <div className="col-6 ">
-            <button onClick={handleEventClick} className="w-100" >Eventi</button>
-            </div>
-          </div>
-          {choise === 'teams' && (
-          <div className="row">
-            {teams.map((team)=>(
-              <div className="col-3 pt-3">
-                <Link to={`/competizioni/${competitionId}/${team.idTeam}`}>
-                <div className="card">
-                 <h1>{team.strTeam}</h1>
-                 <img className="img-fluid" src={team.strBadge}></img>
-                </div>
-                </Link>
-              </div>
-            ))}
-          </div>
-          )}
-          {choise=='event' && (
-            <div>
-              <div className="row">
-              {seasons.map((season)=>(
-                <div className="col-3 text-center">
-                  <button onClick={() => handleSeasonClick(season.strSeason)}>
-                    {season.strSeason}
-                  </button>
-                </div>
-              ))}
-            </div>
-            <div className="row">
-              <h1 className="text-center">{choiseSeason}</h1>
-              {Object.keys(eventsByRound).map(round=>(
-                <div className="col-4 ">
-                  <h2 className="text-center">Giornata {(round)}</h2>
-                  {eventsByRound[round].map(event=>(
-                    <p className="text-center">{event.strHomeTeam} {event.intHomeScore} - {event.intAwayScore} {event.strAwayTeam}</p>
-                  ))}
-                </div>
-              ))}
-            </div>
-            </div>
-            
-          )}
-        </div>
+          <div>Ciao</div>
     )
 }
 
